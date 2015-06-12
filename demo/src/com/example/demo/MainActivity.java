ce9acc0f3a5f6ebf615279a5099862d5b5d2ca2e
KF5 <saintye@kf5.com>
2015-06-12 22:40:46 +0800

    V1.0.2

--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.TextView;
 
+import com.chosen.api.CallBack;
 import com.chosen.model.HelpCenterType;
 import com.chosen.model.KF5SDKConfig;
 
@@ -22,9 +23,24 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		tv1 = (TextView) findViewById(R.id.activity_main_help_btn);
 //		
-		KF5SDKConfig.INSTANCE.init("tianxiang.kf5.com","877977828@qq.com", "111111", 
-				"001552bace471f5b06e40e803231d450366cdc2095621fa0");
+		KF5SDKConfig.INSTANCE.init(MainActivity.this,"tianxiang.kf5.com","877977828@qq.com", "111111", 
+				"001552bace471f5b06e40e803231d450366cdc2095621fa0",new CallBack() {
+					
+					@Override
+					public void onSuccess(String arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onFailure(String arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+				});
 
+		
+		
 		tv1.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -61,17 +77,17 @@ public class MainActivity extends Activity {
 				
 			}
 		});
-////		tv4 = (TextView) findViewById(R.id.activity_main_look_feedback_btn1);
-//		tv4.setOnClickListener(new OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				// TODO Auto-generated method stub
-//				
-//				KF5SDKConfig.INSTANCE.startFeedBackListActivity(MainActivity.this);
-//				
-//			}
-//		});
+		tv4 = (TextView) findViewById(R.id.activity_main_look_feedback_btn1);
+		tv4.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				KF5SDKConfig.INSTANCE.startFeedBackListActivity(MainActivity.this);
+				
+			}
+		});
 //		tv5 = (TextView) findViewById(R.id.activity_main_look_feedback_btn2);
 //		tv5.setOnClickListener(new OnClickListener() {
 //
