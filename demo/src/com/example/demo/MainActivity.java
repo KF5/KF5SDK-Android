e479f570b57b91b4d758d50ec4958ff0d227e94c
KF5 <saintye@kf5.com>
2015-07-06 16:31:16 +0800

    update
    新增图片查看功能，修改图片加载方式。

--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chosen.api.CallBack;
 import com.chosen.model.HelpCenterType;
@@ -22,82 +23,75 @@ public class MainActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		tv1 = (TextView) findViewById(R.id.activity_main_help_btn);
-//		
-		KF5SDKConfig.INSTANCE.init(MainActivity.this,"tianxiang.kf5.com","877977828@qq.com", "111111", 
-				"001552bace471f5b06e40e803231d450366cdc2095621fa0",new CallBack() {
-					
-					@Override
-					public void onSuccess(String arg0) {
-						// TODO Auto-generated method stub
-						
-					}
-					
-					@Override
-					public void onFailure(String arg0) {
-						// TODO Auto-generated method stub
-						
-					}
-				});
+		//		
+		//		KF5SDKConfig.INSTANCE.init(MainActivity.this,"tianxiang.kf5.com","1234567@ncrxsz.org", "111111", 
+		//				"001552bace471f5b06e40e803231d450366cdc2095621fa0",
+		//				new CallBack() {
+		//
+		//			@Override
+		//			public void onSuccess(String arg0) {
+		//				// TODO Auto-generated method stub
+		//
+		//			}
+		//
+		//			@Override
+		//			public void onFailure(String arg0) {
+		//				// TODO Auto-generated method stub
+		//
+		//			}
+		//		});
 
-		
-		
+		KF5SDKConfig.INSTANCE.init(MainActivity.this,"tianxiang.kf5.com","111@kf5.com", 
+				"001557e8f9d8f17765b589e80bbdfa9d23733a6e7a25ead8",new CallBack() {
+
+			@Override
+			public void onSuccess(String result) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, result, 1).show();
+			}
+
+			@Override
+			public void onFailure(String result) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, result, 1).show();
+			}
+		});
+
+
 		tv1.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
-			
-				KF5SDKConfig.INSTANCE.
-				startHelpCenterActivity(MainActivity.this, HelpCenterType.HELPCENTERDEFAULT );
-				
+
+
+				KF5SDKConfig.INSTANCE.startHelpCenterActivity(MainActivity.this, HelpCenterType.HELPCENTERTYPEPOST );
+
 			}
 		});
-		
+
 		tv2 = (TextView) findViewById(R.id.activity_main_feedback_btn);
 		tv2.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
+
 				KF5SDKConfig.INSTANCE.startFeedBackActivity(MainActivity.this);
-				
+
 			}
 		});
-//
+		//
 		tv3 = (TextView) findViewById(R.id.activity_main_look_feedback_btn);
 		tv3.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
-				KF5SDKConfig.INSTANCE.startFeedBackListActivity(MainActivity.this);
-				
-			}
-		});
-		tv4 = (TextView) findViewById(R.id.activity_main_look_feedback_btn1);
-		tv4.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
 				KF5SDKConfig.INSTANCE.startFeedBackListActivity(MainActivity.this);
-				
+
 			}
 		});
-//		tv5 = (TextView) findViewById(R.id.activity_main_look_feedback_btn2);
-//		tv5.setOnClickListener(new OnClickListener() {
-//
-//			@Override
-//			public void onClick(View v) {
-//				// TODO Auto-generated method stub
-//				
-//				KF5SDKConfig.INSTANCE.startFeedBackListActivity(MainActivity.this);
-//				
-//			}
-//		});
 	}
 }
