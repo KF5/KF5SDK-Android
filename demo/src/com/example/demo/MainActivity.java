8c1a29822c8c29ce8dd1b7a9e053b9d5f10e5ea5
KF5 <saintye@kf5.com>
2015-12-31 21:23:16 +0800

    V1.4新增即时交谈功能
    新增即时交谈功能，优化接口功能

--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
 
+import com.kf5chat.ui.KF5ChatActivity;
 import com.kf5sdk.api.CallBack;
 import com.kf5sdk.init.KF5SDKConfig;
 import com.kf5sdk.init.UserInfo;
 import com.kf5sdk.model.HelpCenterType;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,8 +19,8 @@ public class MainActivity extends Activity {
 
 
 	private LinearLayout layoutHelpCenter,layoutFeedBack,layoutLookFeedBack;
-	
-	
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,28 +29,27 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		UserInfo userInfo = new UserInfo();
-		//		userInfo.appId = "00155f5851e24de5079262dda41816a9cd253e165ef799cf";
-		//		userInfo.helpAddress = "joymay.kf5.com";
-		//		userInfo.email = "888888@qq.com";
+		userInfo.appId = "00155bee6f7945ea5aa21c6ffc35f7aa7ed0999d7c6b6029";
+		userInfo.helpAddress = "tianxiang.kf5.com";
+		userInfo.email = "12q33333@qq.com";
 		//		userInfo.password = "1212356465";
 		//		userInfo.name = "科比1234";
-		userInfo.setAppId("001561e7d848e79f55a2e93c3b75024d39dcc1fc4f1cd6cf");
-		userInfo.setEmail("guest@kinglan.info");
-		userInfo.setPassword("123456");
-		userInfo.setHelpAddress("kinglan.kf5.com");
-		userInfo.setName("kcdhrss");
+		//		userInfo.setAppId("001561e7d848e79f55a2e93c3b75024d39dcc1fc4f1cd6cf");
+		//		userInfo.setEmail("guest@kinglan.info");
+		//		userInfo.setHelpAddress("kinglan.kf5.com");
+		//		userInfo.setName("kcdhrss");
 		KF5SDKConfig.INSTANCE.init(MainActivity.this, userInfo, new CallBack() {
 
 			@Override
 			public void onSuccess(String result) {
 				// TODO Auto-generated method stub
-
+				System.out.println("======成功"+result);
 			}
 
 			@Override
 			public void onFailure(String result) {
 				// TODO Auto-generated method stub
-
+				System.out.println("======失败"+result);
 			}
 		});
 
@@ -88,5 +89,17 @@ public class MainActivity extends Activity {
 
 			}
 		});
+		
+		LinearLayout layout = (LinearLayout) findViewById(R.id.chat_center);
+		layout.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				KF5SDKConfig.INSTANCE.startKF5ChatActivity(MainActivity.this);
+//				startActivity(new Intent(getApplicationContext(), KF5ChatActivity.class));
+			}
+		});
+		
 	}
 }
