f6a27c040ac8428c5f06871aa6f0c76d69eee636
KF5 <saintye@kf5.com>
2016-05-11 14:31:03 +0800

    update
    新增图片选择功能，修复部分机型不能正常录音的bug，兼容不同手机版本的emoji表情显示，优化IM消息显示风格。

--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.demo;
 
 
 import com.kf5sdk.api.CallBack;
+import com.kf5sdk.config.KF5ActivityUiConfig;
+import com.kf5sdk.config.KF5SDKActivityUIManager;
 import com.kf5sdk.init.KF5SDKConfig;
 import com.kf5sdk.init.UserInfo;
 import com.kf5sdk.model.HelpCenterType;
 import com.kf5sdk.simpleapp.R;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Layout;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ClickableSpan;
+import android.text.style.URLSpan;
+import android.text.util.Linkify;
+import android.util.Patterns;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -27,19 +43,28 @@ public class MainActivity extends Activity {
 
 	public static final String MESSAGE_RECEIVED_ACTION = "com.example.jpushdemo.MESSAGE_RECEIVED_ACTION";
 
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		activity = this;
-		UserInfo userInfo = new UserInfo();
+		UserInfo userInfo = new UserInfo();	
+	
 
-		userInfo.appId = "00155bxxx5exxxxa5aa21c6ffc35f7aa7ed0999d7c6b6029";
-		userInfo.helpAddress = "xxxxg.kf5.com";
-		userInfo.email = "12q33333@qq.com";
-		userInfo.deviceToken = "";
-		userInfo.name = "测试";
+
+		KF5ActivityUiConfig kf5ActivityUiConfig = new KF5ActivityUiConfig();
+		kf5ActivityUiConfig.topBarBackground = Color.BLUE;
+		kf5ActivityUiConfig.topBarHeight = 200;
+		KF5SDKActivityUIManager.setKf5ActivityUiConfig(kf5ActivityUiConfig);
+				userInfo.appId = "00131211421354f6a8073e3ac7ec00f6a8073e3ac7ec00";
+				userInfo.helpAddress = "xxxx.kf5.com";
+				userInfo.email = "123@qq.com";
+				userInfo.deviceToken = "";
+				userInfo.name = "测试";
+				userInfo.phone = "18712475678";
+
 
 
 		
@@ -54,6 +79,33 @@ public class MainActivity extends Activity {
 				//				String pushID = JPushInterface.getRegistrationID(MainActivity.this);
 				//				System.out.println(pushID+"==========");
 				//				KF5SDKConfig.INSTANCE.savePushToken(activity, pushID);
+				//				KF5SDKConfig.INSTANCE.deletePushToken(MainActivity.this, "1213546654asdzxcasd" , new CallBack() {
+				//
+				//					@Override
+				//					public void onSuccess(final String result) {
+				//						// TODO Auto-generated method stub
+				//						System.out.println("==========成功"+result);
+				//						runOnUiThread(new Runnable() {
+				//							
+				//							@Override
+				//							public void run() {
+				//								// TODO Auto-generated method stub
+				//							try {
+				//								Toast.makeText(activity, new JSONObject(result).getString("message"), 1).show();
+				//							} catch (JSONException e) {
+				//								// TODO Auto-generated catch block
+				//								e.printStackTrace();
+				//							}	
+				//							}
+				//						});
+				//					}
+				//
+				//					@Override
+				//					public void onFailure(String result) {
+				//						// TODO Auto-generated method stub
+				//						System.out.println("==========失败"+result);
+				//					}
+				//				});
 			}
 
 			@Override
@@ -112,4 +164,7 @@ public class MainActivity extends Activity {
 		});
 
 	}
+
+
+
 }
--- demo/src/com/kf5/support/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author chosen
- *
- * @version 创建时间：2015年9月25日  下午4:32:51
- */
-package com.kf5.support;
