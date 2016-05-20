7b57b8f6fe0d6e3abb50e78418602087d4f7f08b
KF5 <saintye@kf5.com>
2016-05-20 17:10:10 +0800

    update
    新增SDK UI自定义功能

--- KF5SDKLibrary/gen/com/chosen/kf5sdk/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.chosen.kf5sdk;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- demo/src/com/example/demo/MainActivity.java
+++ demo/src/com/example/demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 
 import com.kf5sdk.api.CallBack;
+import com.kf5sdk.config.ChatActivityUIConfig;
 import com.kf5sdk.config.ChatAdapterUIConfig;
 import com.kf5sdk.config.FeedBackActivityUIConfig;
 import com.kf5sdk.config.FeedBackDetailsActivityUIConfig;
@@ -9,40 +10,39 @@ import com.kf5sdk.config.FeedBackDetailsAdapterUIConfig;
 import com.kf5sdk.config.HelpCenterActivityUIConfig;
 import com.kf5sdk.config.HelpCenterTypeDetailsActivityUIConfig;
 import com.kf5sdk.config.KF5ActivityUiConfig;
+import com.kf5sdk.config.KF5SDKActivityParamsManager;
 import com.kf5sdk.config.KF5SDKActivityUIManager;
 import com.kf5sdk.config.LookFeedBackAdapterUIConfig;
 import com.kf5sdk.config.OrderAttributeActivityUIConfig;
 import com.kf5sdk.config.OrderAttributeAdapterUIConfig;
+import com.kf5sdk.config.api.ChoiceAttachmentItemClickCallBack;
+import com.kf5sdk.config.api.DialogBtnClickCallBack;
 import com.kf5sdk.config.api.HelpCenterTopRightBtnCallBack;
+import com.kf5sdk.config.api.KF5ChatActivityTopRightBtnCallBack;
+import com.kf5sdk.config.api.RatingDialogItemClickCallBack;
+import com.kf5sdk.config.api.TicketChoiceAttachmentCallBack;
+import com.kf5sdk.config.api.UserDefinedDialogCallBack;
+import com.kf5sdk.config.api.UserDefinedRatingDialogCallBack;
 import com.kf5sdk.init.KF5SDKConfig;
 import com.kf5sdk.init.UserInfo;
 import com.kf5sdk.model.HelpCenterType;
 import com.kf5sdk.simpleapp.R;
+import com.kf5sdk.view.ActionSheetDialog;
+import com.kf5sdk.view.ChatDialog;
+import com.kf5sdk.view.ChatDialog.onClickListener;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.Layout;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.method.LinkMovementMethod;
-import android.text.style.BackgroundColorSpan;
-import android.text.style.ClickableSpan;
-import android.text.style.URLSpan;
-import android.text.util.Linkify;
-import android.util.Patterns;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
+@SuppressLint("ResourceAsColor")
 public class MainActivity extends Activity {
 
 
@@ -63,34 +63,103 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		activity = this;
 		UserInfo userInfo = new UserInfo();	
-		userInfo.appId = "00155beexc3xax6b6029";
-		userInfo.helpAddress = "xxxxxg.kf5.com";
-		userInfo.email = "88xxxxx213@qq.com";
-		userInfo.deviceToken = "";
-		userInfo.name = "测试";
-		userInfo.phone = "1xxxx5678";
+	
+				
+		//				XmlResourceParser xpp=Resources.getSystem().getXml(R.color.button_text);   
+		//				try {  
+		//				     ColorStateList csl= ColorStateList.createFromXml(getResources(),xpp);  
+		//				     btn.setTextColor(csl);  
+		//				} catch (Exception e) {  
+		//				     // TODO: handle exception  
+		//				}  
+		//				Button btn=(Button)findViewById(R.id.btn);  
+		//				Resources resource=(Resources)getBaseContext().getResources();   
+		//				ColorStateList csl=(ColorStateList)resource.getColorStateList(R.color.button_text);  
+		//				if(csl!=null){  
+		//				     btn.setTextColor(color_state_list);//设置按钮文字颜色  
+		//				}  
 
-
-		
 		//全局头部工具栏UI设置
 		KF5ActivityUiConfig kf5ActivityUiConfig = new KF5ActivityUiConfig();
 		//		kf5ActivityUiConfig.topBarBackground = Color.BLUE;
 		kf5ActivityUiConfig.topBarHeight = 200;
-		//		kf5ActivityUiConfig.setTvRightViewTextColor(R.drawable.text_colordrawable);
-		//		kf5ActivityUiConfig.setTvRightViewTextColor(Color.RED);
-		kf5ActivityUiConfig.setTvRightViewTextSize(16);
-		kf5ActivityUiConfig.setTvRightViewTextColorId(R.drawable.text_colordrawable);
-		kf5ActivityUiConfig.setTvTitleTextColor(Color.YELLOW);
-		kf5ActivityUiConfig.setTvTitleTextSize(18);
-		kf5ActivityUiConfig.setBackImgSource(R.drawable.back_img_source);
+//		kf5ActivityUiConfig.setTvRightViewTextSize(16);
+//		kf5ActivityUiConfig.setTvTitleTextColor(Color.YELLOW);
+//		kf5ActivityUiConfig.setTvTitleTextSize(18);
+//		kf5ActivityUiConfig.setBackImgSource(R.drawable.back_img_source);
+		kf5ActivityUiConfig.setTvRightViewBackgroundSoure(R.drawable.chat);
+		kf5ActivityUiConfig.setThemeId(R.style.TestAppTheme);
+		kf5ActivityUiConfig.setTicketChoiceAttachmentCallBack(new TicketChoiceAttachmentCallBack() {
+			
+			@Override
+			public void onChoiceAttachment(Context context,
+					final ChoiceAttachmentItemClickCallBack choiceAttachmentItemClickCallBack) {
+				// TODO Auto-generated method stub
+				
+				new ActionSheetDialog(context)
+				.builder()
+				.setCancelable(true)
+				.setCanceledOnTouchOutside(true)
+				.addSheetItem("相机", ActionSheetDialog.SheetItemColor.Red,
+						new ActionSheetDialog.OnSheetItemClickListener() {
+					@Override
+					public void onClick(int which) {
+						if (choiceAttachmentItemClickCallBack !=  null) {
+							choiceAttachmentItemClickCallBack.capturePictureFromCamera();
+						}
+					}
+				})
+				.addSheetItem("相册", ActionSheetDialog.SheetItemColor.Red,
+						new ActionSheetDialog.OnSheetItemClickListener() {
+					@Override
+					public void onClick(int which) {
+						if (choiceAttachmentItemClickCallBack != null) {
+							choiceAttachmentItemClickCallBack.choicePicturFromFile();
+						}
+					}
+				}).show();
+				
+			}
+		});
+		kf5ActivityUiConfig.setUserDefinedDialogCallBack(new UserDefinedDialogCallBack() {
+			
+			@Override
+			public void onShowUserDefinedDialog(Context context, String title, String content, String leftBtnText,
+					String rightBtnText, DialogBtnClickCallBack dialogBtnClickCallBack) {
+				// TODO Auto-generated method stub
+//				dialogBtnClickCallBack.onPositiveBtnClick();
+				Toast.makeText(context, content, Toast.LENGTH_SHORT).show();
+			}
+		});
+		//		kf5ActivityUiConfig.setHttpRequestLoadingDialogCallBack(new HttpRequestLoadingDialogCallBack() {
+		//
+		//			@Override
+		//			public void onHttpRequestStart(Context context,String content) {
+		//				// TODO Auto-generated method stub
+		//				Toast.makeText(activity, "请求开始"+content, Toast.LENGTH_SHORT).show();
+		//			}
+		//
+		//			@Override
+		//			public void onHttpRequestFinish(Context context) {
+		//				// TODO Auto-generated method stub
+		//				Toast.makeText(activity, "请求结束", Toast.LENGTH_SHORT).show();
+		//			}
+		//		});
 		KF5SDKActivityUIManager.setKf5ActivityUiConfig(kf5ActivityUiConfig);
 
 
-		
+		userInfo.appId = "0015703xxx1145ffa131exxxx73e3ac7ec00";
+		userInfo.helpAddress = "xxxx.kf5.com";
+		userInfo.email = "123@qq.com";
+		userInfo.deviceToken = "";
+		userInfo.name = "测试";
+		userInfo.phone = "187xxxx5679";
+
+
 
 		//工单反馈界面UI设置
 		FeedBackActivityUIConfig feedBackActivityUIConfig = new FeedBackActivityUIConfig();
-		feedBackActivityUIConfig.setImageBtnSource(R.drawable.feed_back);
+		feedBackActivityUIConfig.setImageBtnSource(R.drawable.back_img_source);
 		feedBackActivityUIConfig.setItemAttachmentNameTextColor(Color.BLUE);
 		feedBackActivityUIConfig.setItemAttachmentNameTextSize(20);
 		feedBackActivityUIConfig.setItemImageSource(R.drawable.ic_launcher);
@@ -98,6 +167,11 @@ public class MainActivity extends Activity {
 		feedBackActivityUIConfig.setItemTvRemoveTextSize(10);
 		feedBackActivityUIConfig.setTvCommitContent("点我");
 		feedBackActivityUIConfig.setTvTitleContent("我要反馈");
+		feedBackActivityUIConfig.setEtContentHint("请在这里输入你要反馈的内容");
+		feedBackActivityUIConfig.setEtContentTextColor(getResources().getColor(R.color.et_content_text_color));
+		feedBackActivityUIConfig.setEtHintTextColor(getResources().getColor(R.color.et_hint_text_color));
+		feedBackActivityUIConfig.setEtContentTextSize(18);
+		feedBackActivityUIConfig.setEtBackgroundSource(R.color.et_content_text_color);
 		KF5SDKActivityUIManager.setFeedBackActivityUIConfig(feedBackActivityUIConfig);
 
 
@@ -110,14 +184,17 @@ public class MainActivity extends Activity {
 		feedBackDetailsAdapterUIConfig.setTvDateTextSize(10);
 		feedBackDetailsAdapterUIConfig.setTvNameTextColor(Color.YELLOW);
 		feedBackDetailsAdapterUIConfig.setTvNameTextSize(16);
+		
+		
 		KF5SDKActivityUIManager.setFeedBackDetailsAdapterUIConfig(feedBackDetailsAdapterUIConfig);
 
-		//工单属性界面UI设置
+		//工单属性适配器UI设置
 		OrderAttributeAdapterUIConfig orderAttributeAdapterUIConfig = new  OrderAttributeAdapterUIConfig();
 		orderAttributeAdapterUIConfig.setTvLabelTextColor(Color.GREEN);
 		orderAttributeAdapterUIConfig.setTvLabelTextSize(22);
 		orderAttributeAdapterUIConfig.setTvValueTextColor(Color.YELLOW);
 		orderAttributeAdapterUIConfig.setTvValueTextSize(14);
+	
 		KF5SDKActivityUIManager.setOrderAttributeAdapterUIConfig(orderAttributeAdapterUIConfig);
 
 
@@ -152,11 +229,23 @@ public class MainActivity extends Activity {
 		});
 		KF5SDKActivityUIManager.setHelpCenterActivityUIConfig(helpCenterActivityUIConfig);
 
-
+		//工单属性界面UI设置
+		OrderAttributeActivityUIConfig orderAttributeActivityUIConfig = new OrderAttributeActivityUIConfig();
+		orderAttributeActivityUIConfig.setTvTitleContent("工单属性噢");
+		KF5SDKActivityUIManager.setOrderAttributeActivityUIConfig(orderAttributeActivityUIConfig);
+		
 		//反馈详情界面UI设置
 		FeedBackDetailsActivityUIConfig feedBackDetailsActivityUIConfig = new FeedBackDetailsActivityUIConfig();
 		feedBackDetailsActivityUIConfig.setTvSubmitTextSize(22);
 		feedBackDetailsActivityUIConfig.setTvSubmitTextColor(R.drawable.text_colordrawable);
+		feedBackDetailsActivityUIConfig.setTvSendText("开始发送");
+		feedBackDetailsActivityUIConfig.setEtContentHint("你可以输入你想要的内容");
+//		feedBackDetailsActivityUIConfig.setEtContentTextColor(getResources().getColor(R.color.et_content_text_color));
+//		feedBackDetailsActivityUIConfig.setBackContentLayoutImageSource(R.drawable.ic_launcher);
+//		feedBackDetailsActivityUIConfig.setEtBackgroundSource(R.color.et_content_text_color);
+//		feedBackDetailsActivityUIConfig.setShowChoiceImageDialogImageSource(R.drawable.help_center);
+//		feedBackDetailsActivityUIConfig.setShowImageLayoutImageSource(R.drawable.feed_back);
+//		feedBackDetailsActivityUIConfig.setTvSendBackgroundSource(R.color.et_hint_text_color);
 		KF5SDKActivityUIManager.setFeedBackDetailsActivityUIConfig(feedBackDetailsActivityUIConfig);
 
 		LookFeedBackAdapterUIConfig lookFeedBackAdapterUIConfig = new LookFeedBackAdapterUIConfig();
@@ -168,11 +257,63 @@ public class MainActivity extends Activity {
 		lookFeedBackAdapterUIConfig.setTvTitleTextSize(20);
 		KF5SDKActivityUIManager.setLookFeedBackAdapterUIConfig(lookFeedBackAdapterUIConfig);
 
+		//IM界面UI设置
+		ChatActivityUIConfig chatActivityUIConfig = new ChatActivityUIConfig();
+		chatActivityUIConfig.setTvTicketText("自定义内容");
+		chatActivityUIConfig.setShowPopwindow(false);
+		chatActivityUIConfig.setRatDialogTitle("开始评价,你已经评价成功，请不要再次评价了你已经评价成功，请不要再次评价了你已经");
+		chatActivityUIConfig.setRatTipContentIfRatingSuccess("你已经评价成功，请不要再次评价了");
+		chatActivityUIConfig.setShowContentWhenChatEnd("恋爱已结束");
+		chatActivityUIConfig.setShowContentWhenGetAgent("正在找人搭理你");
+		chatActivityUIConfig.setShowContentWhenNoAgentOnline("没有人打理你");
+		chatActivityUIConfig.setTitleForDialogIfNoAgentOnline("提示一下，没有人搭理你");
+		chatActivityUIConfig.setShowContentWhenConnecting("正在寻找客服噢....");
+//		chatActivityUIConfig.setTvTitleVisible(false);
+		chatActivityUIConfig.setUserDefinedRatingDialogCallBack(new UserDefinedRatingDialogCallBack() {
+			
+			@Override
+			public void onUserDefinedRatingDialogShow(Context context,
+					final RatingDialogItemClickCallBack ratingDialogItemClickCallBack) {
+				// TODO Auto-generated method stub
+				new ChatDialog(context)
+				.setTitle("提示一下")
+				.setMessage("你需要评价一下他的回复")
+				.setLeftButton("不满意", new  onClickListener() {
+					
+					@Override
+					public void onClick(ChatDialog dialog) {
+						// TODO Auto-generated method stub
+						dialog.dismiss();
+						ratingDialogItemClickCallBack.onRatingDialogItemClick(0);
+					}
+				}).setRightButton("确定", new onClickListener() {
+					
+					@Override
+					public void onClick(ChatDialog dialog) {
+						// TODO Auto-generated method stub
+						dialog.dismiss();
+						ratingDialogItemClickCallBack.onRatingDialogItemClick(1);
+					}
+				}).show();
+			}
+		});
+		chatActivityUIConfig.setKf5ChatActivityTopRightBtnCallBack(new KF5ChatActivityTopRightBtnCallBack() {
+
+			@Override
+			public void onTopRightBtnClick(Context context) {
+				// TODO Auto-generated method stub
+				Toast.makeText(context, "这里是自定义回调噢", Toast.LENGTH_SHORT).show();
+			}
+		});
+		KF5SDKActivityUIManager.setChatActivityUIConfig(chatActivityUIConfig);
+		
+		
 		KF5SDKConfig.INSTANCE.init(MainActivity.this, userInfo, new CallBack() {
 
 			@Override
 			public void onSuccess(String result) {
 				// TODO Auto-generated method stub
+				Toast.makeText(activity, "登录成功", Toast.LENGTH_SHORT).show();
 				//				Toast.makeText(getApplicationContext(), "�ɹ�"+ result, 1).show();
 				//				String pushID = JPushInterface.getRegistrationID(MainActivity.this);
 				//				System.out.println(pushID+"==========");
@@ -263,6 +404,13 @@ public class MainActivity extends Activity {
 
 	}
 
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		KF5SDKActivityUIManager.resetActivityUIConfig();
+		KF5SDKActivityParamsManager.resetActivityParamsConfig();
+	}
 
 
 }
